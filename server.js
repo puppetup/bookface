a232aaf9180352f5c3eb1bded9adf02038440f12
puppetup <savinohoshizaki@gmail.com>
2022-07-30 22:21:27 -0700

    changed url

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.static('public'));
 
 app.use(require('./routes'));
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network-api', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/bookface', {
   useFindAndModify: false,
   useNewUrlParser: true,
   useUnifiedTopology: true
