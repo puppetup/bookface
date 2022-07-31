6c44c77170679457fd70340860122017d0b8b01e
puppetup <savinohoshizaki@gmail.com>
2022-07-30 21:45:42 -0700

    added some stuffff

--- models/index.js
+++ models/index.js
@@ -1,4 +1,4 @@
-const User = require('./Users');
-const Thought = require('./Thoughts');
+const Users = require('./Users');
+const Thoughts = require('./Thoughts');
 
 module.exports = { Users, Thoughts };
